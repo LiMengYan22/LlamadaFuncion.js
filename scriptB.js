8826e2544f60b61e7c450060a4fdec73950edf8e
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-23 13:23:58 +0100

    Create scriptB.js

---
+++ scriptB.js
@@ -0,0 +1,11 @@
+<script type="text/javascript"><!--
+function f2(){
+alert('Soy una función de P2');
+}
+//--></script>
+
+// y este otro HTML responde:
+<h1>p2</h1>
+<p id="saludo">
+  Soy un texto de P2
+    </p>
